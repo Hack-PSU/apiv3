70b7ea125efb6a5c35954f1b4d763ad061971c58
susanto-tm <susanto.tm@gmail.com>
2023-03-02 11:29:49 -0500

    Added alternative firebase configuration for staging.

--- src/common/config/firebase.config.ts
+++ src/common/config/firebase.config.ts
@@ -7,19 +7,23 @@ export const firebaseConfig = registerAs<FirebaseConfig>(
   ConfigToken.GCP,
   () => {
     if (process.env.GOOGLE_CERT) {
-      let serviceAccount: any;
-
       if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
-        serviceAccount = process.env.GOOGLE_CERT;
+        return {
+          credential: admin.credential.cert({
+            projectId: String(process.env.GOOGLE_CERT_PROJECT_ID),
+            privateKey: String(process.env.GOOGLE_CERT_PRIVATE_KEY),
+            clientEmail: String(process.env.GOOGLE_CERT_CLIENT_EMAIL),
+          }),
+          storageBucket: `${process.env.STAGING_STORAGE}.appspot.com`,
+        };
       } else {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
-        serviceAccount = require(process.env.GOOGLE_CERT);
+        const serviceAccount = require(process.env.GOOGLE_CERT);
+        return {
+          credential: admin.credential.cert(serviceAccount),
+          storageBucket: `${process.env.STAGING_STORAGE}.appspot.com`,
+        };
       }
-
-      return {
-        credential: admin.credential.cert(serviceAccount),
-        storageBucket: `${process.env.STAGING_STORAGE}.appspot.com`,
-      };
     } else {
       return {
         credential: admin.credential.applicationDefault(),
