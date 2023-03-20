fb1987c70537f453d6d7e4a5237c8373cc49fc77
susanto-tm <susanto.tm@gmail.com>
2023-03-20 18:23:01 -0400

    Fixed debugging configuration.

--- src/common/config/apple.config.ts
+++ src/common/config/apple.config.ts
@@ -1,22 +1,16 @@
 import { registerAs } from "@nestjs/config";
 import { AppleAuthConfig } from "common/apple/apple-auth.types";
 import { ConfigToken } from "common/config/config.constants";
-import * as fs from "fs";
 
 export const appleConfig = registerAs<AppleAuthConfig>(
   ConfigToken.APPLE,
   () => {
-    let pk = process.env.APPLE_AUTH_PK;
-    if (process.env.GOOGLE_CERT) {
-      pk = fs.readFileSync(process.env.APPLE_AUTH_PK).toString();
-    }
-
     return {
       kid: process.env.APPLE_AUTH_KID,
       iss: process.env.APPLE_AUTH_ISS,
       aud: process.env.APPLE_AUTH_AUD,
       sub: process.env.APPLE_AUTH_SUB,
-      pk: pk,
+      pk: process.env.APPLE_AUTH_PK,
     };
   },
 );
