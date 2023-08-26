c66e23f26a2faeb5f1e9cc910dfd9af3ca0c440c
John Durrell <john.m.durrell@gmail.com>
2023-08-26 00:11:56 -0400

    Remove audience validation to check if this is the problem.

--- src/common/gcp/auth/firebase-auth.strategy.ts
+++ src/common/gcp/auth/firebase-auth.strategy.ts
@@ -16,7 +16,7 @@ export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: configService.get("JWT_AUTH_AUDIENCE"),
+      // audience: configService.get("JWT_AUTH_AUDIENCE"),
       ignoreExpiration: false,
       secretOrKeyProvider: (request, rawJwtToken, done) => {
         const jwtHeader = jwtDecode(rawJwtToken, {
@@ -36,7 +36,7 @@ export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
       },
     });
 
-    console.log(configService.get("JWT_AUTH_AUDIENCE"));
+    console.log(`jwt_auth_audience: ${configService.get("JWT_AUTH_AUDIENCE")}`);
   }
 
   validate(payload: unknown): unknown {
