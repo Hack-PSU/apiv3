c68cc189397cac422c228548117b4d1f795b6300
John Durrell <john.m.durrell@gmail.com>
2024-02-12 18:28:00 -0500

    Update yaml for one-time artifact push before deployment.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocsModule } from "common/docs/docs.module";
 import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
+  console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
     .setTitle("HackPSU Documentation")
