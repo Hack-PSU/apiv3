30efcb5457d489703218db6a4e584a5b82ac8442
s9kt <psinghal1@protonmail.com>
2024-10-07 12:25:53 -0400

    fixed file validation and got the parsing to work

--- src/modules/judging/project.service.ts
+++ src/modules/judging/project.service.ts
@@ -2,10 +2,29 @@ import { Injectable } from "@nestjs/common";
 
 import fs from "fs"
 import { parse } from "csv-parse"
+import { lookup } from "mime";
 
 @Injectable()
 export class ProjectService {
     async parseProjectFile(file: Express.Multer.File) {
-        console.log(file.mimetype)
+        let records = []
+
+        parse(file.buffer.toString(), {
+            delimiter: ",",
+        })
+            .on("data", (row) => {
+                // console.log(row)
+                records.push(row)
+            })
+            .on("end", () => {
+                parsedCsv(records)
+            })
+            .on("error", (err) => {
+                console.error(err)
+            })
+
+        function parsedCsv(arr: any[]) {
+            
+        }
     }
 }
