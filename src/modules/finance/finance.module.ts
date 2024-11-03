ff09a8ad5298ed48bd1fc9771f0b2914f3ff2e29
s9kt <psinghal1@protonmail.com>
2024-11-03 14:12:24 -0500

    add provider

--- src/modules/finance/finance.module.ts
+++ src/modules/finance/finance.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { FinanceController } from "./finance.controller";
+import { FinanceService } from "./finance.service";
 
 @Module({
-    controllers: [FinanceController]
+    controllers: [FinanceController],
+    providers: [FinanceService]
 })
 export class FinanceModule {}
