dc8504044e95d96f7e1d9d27a466ba2e91f704f7
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2024-11-07 14:35:42 -0500

    fmt + define Invoice bucket config

--- src/common/config/invoice.config.ts
+++ src/common/config/invoice.config.ts
@@ -3,10 +3,10 @@ import { InvoiceBucketConfig } from "common/gcp";
 import { ConfigToken } from "common/config/config.constants";
 
 export const invoiceBucketConfig = registerAs<InvoiceBucketConfig>(
-    ConfigToken.INVOICE,
-    () => {
-        return {
-            invoice_bucket: process.env.INVOICE_BUCKET,
-        };
-    },
+  ConfigToken.INVOICE,
+  () => {
+    return {
+      invoice_bucket: process.env.INVOICE_BUCKET,
+    };
+  },
 );
