39027a46c11dcdb17b453a1db2d3416c095f5ac8
s9kt <psinghal1@protonmail.com>
2024-11-09 14:45:03 -0500

    feat: finance service bucket methods

--- src/modules/finance/finance.service.ts
+++ src/modules/finance/finance.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { InvoiceBucketConfig } from "common/gcp";
+import * as admin from "firebase-admin";
+import { ConfigToken } from "common/config";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
-export class FinanceService {}
+export class FinanceService {
+    private invoiceBucketName: string;
+
+    constructor(private readonly configService: ConfigService) {
+        this.invoiceBucketName = configService.get<InvoiceBucketConfig>(
+            ConfigToken.INVOICE,
+        ).invoice_bucket;
+    }
+
+    private get invoiceBucket() {
+        return admin.storage().bucket(this.invoiceBucketName);
+    }
+
+    private getInvoiceFileName(userId: string): string {
+        return `${userId}.pdf`;
+    }
+
+    private getAuthenticatedResumeUrl(filename: string): string {
+        return `https://storage.cloud.google.com/${this.invoiceBucketName}/${filename}`;
+    }
+}
