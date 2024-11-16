1a703fb83bb50c5b6b4c0c3d0e0c87a08999387b
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2024-11-16 04:12:06 -0500

    I think this fills out the cheque request form. I need to sleep

--- src/modules/finance/finance.controller.ts
+++ src/modules/finance/finance.controller.ts
@@ -3,9 +3,13 @@ import {
   Body,
   Controller,
   Get,
+  Header,
   NotFoundException,
   Post,
   ValidationPipe,
+  StreamableFile,
+  Param,
+  Req,
 } from "@nestjs/common";
 import { ApiTags, OmitType } from "@nestjs/swagger";
 import { ApiDoc } from "common/docs";
@@ -21,6 +25,8 @@ import { Hackathon } from "entities/hackathon.entity";
 import { Organizer } from "entities/organizer.entity";
 import { User } from "entities/user.entity";
 import { nanoid } from "nanoid";
+import { FinanceService } from "./finance.service";
+import { Request } from "express";
 
 class FinanceCreateEntity extends OmitType(FinanceEntity, [
   "id",
@@ -41,6 +47,7 @@ export class FinanceController {
     private readonly userRepo: Repository<User>,
     @InjectRepository(Organizer)
     private readonly organizerRepo: Repository<Organizer>,
+    private readonly financeService: FinanceService,
   ) {}
 
   @Get("/")
@@ -103,5 +110,66 @@ export class FinanceController {
     };
 
     return this.financeRepo.createOne(newFinance).exec();
+  }
+
+  @Get("/:id/cheque")
+  @Header("Content-Type", "application/pdf")
+  async getCheque(@Param("id") id: string): Promise<StreamableFile> {
+    const finance = await this.financeRepo.findOne(id).exec();
+
+    if (!finance) {
+      throw new NotFoundException("Financial record not found");
+    }
+
+    if (finance.status !== Status.APPROVED) {
+      throw new BadRequestException("Financial record not approved");
+    }
+
+    let payee: string;
+
+    if (finance.submitterType === SubmitterType.USER) {
+      const user = await this.userRepo.findOne(finance.submitterId).exec();
+      if (!user) {
+        throw new NotFoundException("User not found");
+      }
+
+      payee = user.firstName + " " + user.lastName;
+    } else if (finance.submitterType === SubmitterType.ORGANIZER) {
+      const organizer = await this.organizerRepo
+        .findOne(finance.submitterId)
+        .exec();
+
+      if (!organizer) {
+        throw new NotFoundException("Organizer not found");
+      }
+      payee = organizer.firstName + " " + organizer.lastName;
+    }
+
+    // Call the service method to generate the PDF bytes
+    const pdfBytes = await this.financeService.populateTemplate(
+      "cheque-request-form",
+      {
+        "UNRESTRICTED  30": true,
+        ORGACCT: 1657,
+        FS1: 30,
+        "Amount 1": finance.amount,
+        TOTAL: finance.amount,
+        ORGANIZATION: "HackPSU",
+        "PAYEE please print clearly": payee,
+        "MAILING ADDRESS If applicable 1": finance.street,
+        "MAILING ADDRESS If applicable 2":
+          finance.city + ", " + finance.state + " " + finance.postalCode,
+        EMAIL: "finance@hackpsu.org",
+        "Description 1": finance.description,
+        "Object Code 1": finance.category,
+        "Today's Date 1_af_date": new Date().toLocaleDateString(),
+        Group1: "Choice2",
+      },
+      finance.id,
+    );
+
+    const pdfBuffer = Buffer.from(pdfBytes);
+
+    return new StreamableFile(pdfBuffer);
   }
 }
