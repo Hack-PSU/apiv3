5f8590a0f7c0c0f78ea8d5a1d9ace6e0d05149b6
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-08-18 09:52:18 +0530

    Add teams to flow

---
+++ src/entities/team.entity.ts
@@ -0,0 +1,65 @@
+import { ApiProperty, PickType } from "@nestjs/swagger";
+import { Column, ID, Table } from "common/objection";
+import { Entity } from "entities/base.entity";
+import { IsBoolean, IsOptional, IsString } from "class-validator";
+
+@Table({
+  name: "teams",
+})
+export class Team extends Entity {
+  @ApiProperty()
+  @IsString()
+  @ID({ type: "string" })
+  id: string;
+
+  @ApiProperty()
+  @IsString()
+  @Column({ type: "string" })
+  name: string;
+
+  @ApiProperty({ type: "string", required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  @Column({ type: "string", required: false, nullable: true })
+  member1?: string;
+
+  @ApiProperty({ type: "string", required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  @Column({ type: "string", required: false, nullable: true })
+  member2?: string;
+
+  @ApiProperty({ type: "string", required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  @Column({ type: "string", required: false, nullable: true })
+  member3?: string;
+
+  @ApiProperty({ type: "string", required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  @Column({ type: "string", required: false, nullable: true })
+  member4?: string;
+
+  @ApiProperty({ type: "string", required: false, nullable: true })
+  @IsOptional()
+  @IsString()
+  @Column({ type: "string", required: false, nullable: true })
+  member5?: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @Column({ type: "boolean" })
+  isActive: boolean;
+}
+
+export class TeamEntity extends PickType(Team, [
+  "id",
+  "name",
+  "member1",
+  "member2",
+  "member3",
+  "member4",
+  "member5",
+  "isActive",
+] as const) {}
