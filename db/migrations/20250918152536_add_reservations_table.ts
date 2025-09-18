2c09543c09d7be49d9c5cee597d15aed5fa2394a
joeboppell <joeybops1@gmail.com>
2025-09-18 11:40:41 -0400

    migrations for location capacity and reservations table

---
+++ db/migrations/20250918152536_add_reservations_table.ts
@@ -0,0 +1,55 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("reservations", (table) => {
+        table
+            .increments("id")
+            .primary()
+            .notNullable();
+        table
+            .bigInteger("start_time")
+            .unsigned()
+            .notNullable();
+
+        table
+            .bigInteger("end_time")
+            .unsigned()
+            .notNullable();
+
+        table
+            .integer("location_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("locations")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+
+        table
+            .uuid("team_id")
+            .notNullable()
+            .references("id")
+            .inTable("teams")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        
+        table
+            .uuid("hackathon_id")
+            .notNullable()
+            .references("id")
+            .inTable("hackathons")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        
+        table
+            .enu("reservation_type", ["participant", "admin"])
+            .notNullable();
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists("reservations");
+}
+
