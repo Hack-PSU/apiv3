0235b8f30a1fd1d9f391a7eb55b43d60e3117a31
CruidGals <kchiem30@gmail.com>
2025-09-23 20:30:18 -0400

    Add new variables in location.entity.ts

--- src/entities/location.entity.ts
+++ src/entities/location.entity.ts
@@ -1,7 +1,7 @@
 import { Column, ID, Table } from "common/objection";
 import { Entity } from "entities/base.entity";
 import { ApiProperty, PickType } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsString, IsBoolean, IsNumber } from "class-validator";
 
 @Table({
   name: "locations",
@@ -16,6 +16,31 @@ export class Location extends Entity {
   @IsString()
   @Column({ type: "string" })
   name: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @Column({ type: "boolean", required: true })
+  isBookable: boolean = false;
+
+  @ApiProperty()
+  @IsNumber()
+  @Column({ type: "integer", required: true })
+  teamCapacity: number = 1;
+
+  @ApiProperty()
+  @IsNumber()
+  @Column({ type: "integer", required: true })
+  minReservationMins: number = 30;
+
+  @ApiProperty()
+  @IsNumber()
+  @Column({ type: "integer", required: true })
+  maxReservationMins: number = 120;
+
+  @ApiProperty()
+  @IsNumber()
+  @Column({ type: "integer", required: true })
+  bufferMins: number = 0;
 }
 
 export class LocationEntity extends PickType(Location, [
