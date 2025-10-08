8e653b952bb7d84328a88d88ede8e4b03d658205
joeboppell <joeybops1@gmail.com>
2025-10-08 01:47:39 -0400

    removed uuid from createReservation

--- src/modules/reservation/reservation.service.ts
+++ src/modules/reservation/reservation.service.ts
@@ -60,7 +60,6 @@ export class ReservationService {
 
     const reservation = await this.reservationRepo
       .createOne({
-        id: uuidv4(),
         locationId: data.locationId,
         teamId: data.teamId,
         startTime: data.startTime,
