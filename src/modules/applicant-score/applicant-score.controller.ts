8a4d7c44f71836ee2cbc1e4627e6715d319d420b
Vishnu Venugopal <vishnuvgn05@gmail.com>
2026-02-11 11:49:55 -0500

    skeleton built, need to test

---
+++ src/modules/applicant-score/applicant-score.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { ApiDoc } from "common/docs";
+import { Role } from "common/gcp";
+import { ApplicantScoreService } from "./applicant-score.service";
+import { BulkApplicantScoreDto } from "./dto/applicant-score.dto";
+import { ApplicantScoreEntity } from "entities/applicant-score.entity";
+
+@ApiTags("Applicants")
+@Controller("applicants")
+export class ApplicantScoreController {
+  constructor(private readonly applicantScoreService: ApplicantScoreService) {}
+
+  @Post("/bulk")
+  @ApiDoc({
+    summary: "Overwrite all applicant scores",
+    auth: Role.NONE, // NOTE: check w joe about this. what permissions...
+    response: {
+      created: { type: [ApplicantScoreEntity] },
+    },
+  })
+  async bulkUpload(
+    @Body(new ValidationPipe({ transform: true })) body: BulkApplicantScoreDto,
+  ) {
+    return this.applicantScoreService.overwriteAll(body.scores);
+  }
+}
