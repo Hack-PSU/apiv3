e4dd488a17f3cb4ea1c401fa2397e945b101c62f
CruidGals <kchiem30@gmail.com>
2026-02-17 10:55:38 -0500

    Complete base code

--- src/modules/registration/registration.controller.ts
+++ src/modules/registration/registration.controller.ts
@@ -5,7 +5,7 @@ import { Registration, RegistrationEntity, ApplicationStatus } from "entities/re
 import { ApiProperty, ApiTags } from "@nestjs/swagger";
 import { Role, Roles } from "common/gcp";
 import { ApiDoc } from "common/docs";
-import { IsBoolean, IsEnum, IsOptional } from "class-validator";
+import { ArrayMinSize, IsArray, IsBoolean, IsEnum, IsOptional, IsString } from "class-validator";
 import { Transform } from "class-transformer";
 import { User } from "entities/user.entity";
 import { SendGridService, DefaultTemplate, DefaultFromEmail } from "common/sendgrid";
@@ -16,6 +16,17 @@ class UpdateStatusDto {
   status: ApplicationStatus;
 }
 
+class UpdateStatusBulkDto {
+  @ApiProperty({ type: [String] })
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1) // Must be updating at least one user
+  userIds: string[];
+
+  @ApiProperty({ enum: ApplicationStatus })
+  @IsEnum(ApplicationStatus)
+  status: ApplicationStatus;
+}
 
 class ActiveRegistrationParams {
   @ApiProperty()
@@ -182,6 +193,121 @@ export class RegistrationController {
     await registration.$query().patch(updateData);
 
     return registration.$query();
+  }
+
+  @Patch("/:userId/application-status-bulk")
+  @Roles(Role.NONE)
+  @ApiDoc({
+    summary: "Bulk Update Application Status",
+    auth: Role.NONE,
+    response: {
+      ok: { type: [RegistrationEntity] },
+    },
+  })
+  async updateApplicationStatusBulk(
+    @Body(new ValidationPipe()) body: UpdateStatusBulkDto
+  ) {
+    const registrations: Registration[] = await this.registrationRepo
+      .findAll()
+      .byHackathon()
+      .whereIn("userId", body.userIds);
+
+    if (registrations.length != body.userIds.length) {
+      throw new NotFoundException(`Not all registrations could be retrieved.`);
+    }
+
+    const updateData: Partial<Registration> = {
+      applicationStatus: body.status,
+    };
+
+    if (body.status === ApplicationStatus.ACCEPTED) {
+      const now = new Date();
+      const oneWeekFromNow = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+      updateData.acceptedAt = now.getTime();
+      updateData.rsvpDeadline = oneWeekFromNow.getTime();
+
+      const activeHackathonName = await Hackathon.query().findOne({ active: true }).select("name").first();
+      const users = await this.userRepo.findAll().byHackathon().whereIn("userId", body.userIds);
+      if (
+        users.length == body.userIds &&
+        process.env.RUNTIME_INSTANCE &&
+        process.env.RUNTIME_INSTANCE === "production"
+      ) {
+        // Build the email to each user
+        const messages = users.map(async (user) => {
+          const message = await this.sendGridService.populateTemplate(
+            DefaultTemplate.participantAccepted,
+            {
+              previewText: `You've been accepted to HackPSU ${activeHackathonName.name}!`,
+              date: "March 28-29, 2026",
+              address: "ECore Building, University Park PA",
+              firstName: user.firstName,
+              hackathon: activeHackathonName.name,
+            },
+          );
+
+          return await this.sendGridService.send({
+            from: DefaultFromEmail,
+            to: user.email,
+            subject: `ACTION REQUIRED: RSVP for HackPSU ${activeHackathonName.name}!`,
+            message,
+          });
+        });
+        
+        // Send all the message emails
+        await Promise.all(messages);
+      }
+    }
+
+    if(body.status == ApplicationStatus.REJECTED) {
+
+      if (
+      process.env.RUNTIME_INSTANCE &&
+      process.env.RUNTIME_INSTANCE === "production"
+      ){
+
+        const users = await this.userRepo.findAll().byHackathon().whereIn("userId", body.userIds);
+        const activeHackathonName = await Hackathon.query().findOne({ active: true }).select("name").first();
+        if (users.length == body.userIds.length) {
+          try {
+            // Build the email to each user
+            const messages = users.map(async (user) => {
+              const message = await this.sendGridService.populateTemplate(
+                DefaultTemplate.participantRejected,
+                {
+                  firstName: user.firstName,
+                  hackathon: activeHackathonName.name
+                },
+              );
+
+              return await this.sendGridService.send({
+                from: DefaultFromEmail,
+                to: user.email,
+                subject: "Update regarding your HackPSU application",
+                message,
+              });
+            });
+          } catch (error) {
+            console.error(`Failed to send rejection emails to ${body.userIds}:`, error);
+          }
+        }
+      }
+
+    }
+
+    if(body.status === ApplicationStatus.CONFIRMED || body.status === ApplicationStatus.DECLINED) {
+      updateData.rsvpAt = new Date().getTime();
+    }
+    
+    // Get the ids from registration
+    const ids = registrations.map(reg => reg.id);
+
+    // Perform a batch patch update
+    await Registration.query().whereIn('id', ids).patch(updateData);
+
+    // Return updated records
+    await Registration.query().whereIn('id', ids)
   }
 
 }
